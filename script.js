8b2d4bfe22ff7738035864c70f5ddb985558a203
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-27 11:16:54 +0000

    dom elements query selectors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Variables
 let xp = 0;
 let health = 100;
 let gold = 50;
@@ -5,3 +6,15 @@ let currentWeaponIndex = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+// DOM Elements query selectors
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
