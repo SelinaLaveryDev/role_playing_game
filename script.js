163ce6e8a5688a939c46df770bdfea1f8cb39ad7
Selina Lavery <selinalavery@Selinas-MacBook-Pro.local>
2025-02-27 22:00:37 +0000

    defeatMonster function and kill monster location object

--- script.js
+++ script.js
@@ -76,6 +76,16 @@ const locations = [
     "button functions": [attack, dodge, goTown],
     text: "You are fighting a monster.",
   },
+  {
+    name: "kill monster",
+    "button text": [
+      "Go to town square",
+      "Go to town square",
+      "Go to town square",
+    ],
+    "button functions": [goTown, goTown, goTown],
+    text: "The monster screams Arg! as it dies. You gain experience points and find gold.",
+  },
 ];
 
 // initialize buttons
@@ -190,7 +200,16 @@ function attack() {
     defeatMonster();
   }
 }
-function dodge() {}
+function dodge() {
+  text.innerText =
+    "You dodge the attack from the " + monsters[fighting].name + ".";
+}
 
-function defeatMonster() {}
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
 function lose() {}
